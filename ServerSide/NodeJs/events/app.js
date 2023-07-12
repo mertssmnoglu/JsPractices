16e64ef5903d9f528c1b173fdf9ab36af8655f66
Mert Şişmanoğlu <mert190737fb@gmail.com>
2023-07-12 12:54:56 +0300

    feat: Add node events practices

---
+++ ServerSide/NodeJs/events/app.js
@@ -0,0 +1,10 @@
+const Logger = require('./logger')
+const logger = new Logger()
+
+logger.on('messageLogged', function (data) {
+    console.log('Message Logged', data)
+})
+
+logger.log('mesaj')
+
+
